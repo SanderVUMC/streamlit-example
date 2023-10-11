c3844370b742cf6f58522de9df36a084798dd4f4
Wu-Chen Su <wu-chensu@NLDMIMX9KFV3NV6.local>
2023-10-11 09:00:36 -0500

    update files

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,97 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# Set page title and icon
+from st_pages import Page, show_pages, add_page_title
 
-    Point = namedtuple('Point', 'x y')
-    data = []
 
-    points_per_turn = total_points / num_turns
+st.set_page_config(
+    page_title="Parcel Review Tool",
+    page_icon="👋",
+    layout="wide",
+)
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+show_pages(
+    [
+        Page("app.py", "Home", "🏠"),
+        Page("pages/parcel_review.py", "Parcel Review","👉"),
+        
+    ]
+)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+
+st.title("Parcel Review Tool Homepage")
+
+st.write("""
+## Overview
+The Parcel Review Tool homepage allows users to lookup details on a specific parcel ID, view any associated appeals data, analyze value changes compared to the neighborhood, and find similar parcels in the same neighborhood.
+""")
+
+st.write("""
+## Data
+The tool loads the following data:
+""")
+
+st.write("- `parcels` - Contains details on property parcels such as address, neighborhood, beds/baths, square footage, etc.")
+st.write("- `appeals` - Contains data on property assessment appeals, including parcel ID, appeal reason, and case status.")
+st.write("- `triennial` - Contains assessed values and value changes for the 2022 and 2023 tax years.")
+
+st.write("""
+## Usage
+""")
+
+st.write("""
+### Selecting a Parcel ID  
+On the left sidebar, select either "Input Parcel ID" or "Appealed Parcels":
+""")
+
+st.write("- **Input Parcel ID**: Manually enter a parcel ID number to lookup")
+st.write("- **Appealed Parcels**: Select from a dropdown of parcel IDs that have associated appeal cases. This filters specifically for parcels with appeals cases marked as \"In Review\".")
+
+st.write("Once a parcel ID is selected, details on that parcel will be displayed.")
+
+st.write("""
+### Viewing Parcel Details
+The parcel details section shows information retrieved from the `parcels` dataframe for that specific parcel ID. This includes fields like:
+""")
+
+st.write("- PARID")
+st.write("- Year Built")  
+st.write("- Beds")
+st.write("- Baths")
+st.write("- Square Feet")
+st.write("- Grade")
+st.write("- Exterior Walls")
+st.write("- and more...")
+
+st.write("Any associated appeal cases for that parcel ID are also shown under \"Appeal Information\" if available.")
+
+
+st.write("""
+### Analyzing Value Changes
+The value change analysis compares the current 2023 value to the prior 2022 value and calculates the net change, percent change, and estimated tax change per year.
+
+This is also compared to summary statistics on value changes across the entire neighborhood, such as minimum, maximum, median, and average changes. A histogram visualizes the distribution of percent changes in the neighborhood with the current parcel's change highlighted.
+""")
+
+st.write("""
+### Finding Similar Parcels
+The "Find Similar Parcels" section allows the user to define criteria like year built, beds, baths, square feet, grade, and exterior walls to search for similar parcels within the same neighborhood.  
+
+Sliders and dropdowns let the user refine the search criteria. Matching parcels are displayed in a table with core fields like year built, style, beds/baths, sale price, total value, and more. The target parcel is shown first, followed by the matched parcels.
+""")
+
+st.write("""
+## Interpreting Results
+The tool provides a quick way to see how a specific parcel compares to the broader neighborhood in terms of value changes. The similar parcels table helps identify other properties that may warrant closer comparison for appeals or analysis. Insights from the tool can help guide next steps and priorities for further investigation.
+""")
+
+st.write("""
+---
+**Last Data Update:** October 08, 2023
+
+**Last Code Update:** October 10, 2023
+""")
